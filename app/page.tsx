9f465bbb8d2accc3b77505945e3dc0c13de5ed6c
Eka Gibran Hasany <ekagibran@gmail.com>
2023-08-29 01:20:48 +0200

    fix: change apostrophe character

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
         <div>
-          <p className='mb-6 text-xl'>Hi, I'm Eka, also known as Gibran</p>
+          <p className='mb-6 text-xl'>Hi, I&apos;m Eka, also known as Gibran</p>
           <p>This is My personal website LOL</p>
         </div>
       </div>
